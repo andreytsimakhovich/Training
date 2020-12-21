11c514a17a8850cce1a93733a8a14e4bf50cc1b0
Andrei Tsimakhovich <taa421992@gmail.com>
2020-12-21 09:49:16 +0300

    Add training repository.

---
+++ Training/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int a = int.Parse(Console.ReadLine());
+
+            switch (a)
+            {
+                case 1:
+                    Console.WriteLine("\nВы ввели число 1");
+                    break;
+
+                case 2:
+                    Console.WriteLine("\nВы ввели число 2");
+                    break;
+
+                default:
+                    Console.WriteLine("\nВы ввели неверные данные");
+                    break;
+            }
+        }
+    }
+}
