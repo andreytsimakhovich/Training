2541bac702f8af58b713fcabda603bc77403a88e
Andrei Tsimakhovich <taa421992@gmail.com>
2020-12-25 19:26:36 +0300

    Add

---
+++ two-dimensional_arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace two_dimensional_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Заполнение двумерного массива случайными чсилами<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+            int[,] myArray = new int[10, 6];
+
+            Random random = new Random();
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    myArray[i, j] = random.Next(100);
+                }
+            }
+
+
+            for (int y = 0; y < myArray.GetLength(0); y++)
+            {
+                for (int x = 0; x < myArray.GetLength(1); x++)
+                {
+                    Console.Write(myArray[y, x] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
