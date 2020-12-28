95f11805b409a9ab996deadc387ba27a9c14538b
Andrei Tsimakhovich <taa421992@gmail.com>
2020-12-28 12:49:25 +0300

    Add three-dimensional array.

---
+++ Three-dimensional_array/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Three_dimensional_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int[,,] myArray = new int[4, 3, 5];
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    for (int k = 0; k < myArray.GetLength(2); k++)
+                    {
+                        myArray[i, j, k] = random.Next(100);
+                    }
+                }
+            }
+
+            for (int i = 0; i < myArray.GetLength(0); i++)
+            {
+                Console.WriteLine("Page №: " + (i + 1));
+
+                for (int j = 0; j < myArray.GetLength(1); j++)
+                {
+                    for (int k = 0; k < myArray.GetLength(2); k++)
+                    {
+                        Console.Write(myArray[i, j, k] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine("====================");
+            }
+            Console.ReadKey();
+        }
+    }
+}
