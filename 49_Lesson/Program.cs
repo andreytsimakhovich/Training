6558c4c89473acce4a8ad0ed2692f9b7a05abadd
Andrei Tsimakhovich <taa421992@gmail.com>
2021-01-19 13:34:52 +0300

    Add 49_Lesson, 49_Lesson2.

---
+++ 49_Lesson/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+
+namespace _49_Lesson
+{
+    /*
+         * Рекурсия
+         * 
+         * Методы и стек
+         * 
+         * Переполнение стека
+         */
+
+
+    class Program
+    {
+
+        static void Foo(int i)
+        {
+            Console.WriteLine(i);
+            if (i >= 3)
+                return;
+
+            i++;
+            Foo(i);
+        }
+     
+        static void Main(string[] args)
+        {
+            Foo(0);
+        }
+    }
+}
---
+++ 49_Lesson2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _49_Lesson2
+{
+    // Пример, когда удобно использовать рекурсию
+    class Program
+    {
+        class Item
+        {
+            public int Value { get; set; }
+
+            public Item Child { get; set; }
+        }
+
+        static Item InitItem()
+        {
+            return new Item()
+            {
+                Value = 5,
+                Child = new Item()
+                {
+                    Value = 10,
+                    Child = new Item()
+                    {
+                    Value = 2
+                    }
+                }
+                
+            };
+        }
+
+        static void Print (Item item)
+        {
+            if (item != null)
+            {
+                Console.WriteLine(item.Value);
+                Print(item.Child);
+            }
+        
+        }
+        static void Main(string[] args)
+        {
+            Item item = InitItem();
+            Print(item);
+        }
+    }
+}
