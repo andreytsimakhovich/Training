3cbba0490cc9bf8c8421a0bcfcc28fc4d52d782c
Andrei Tsimakhovich <taa421992@gmail.com>
2021-01-19 16:35:00 +0300

    Add 49_Lesson_Task1.

---
+++ 49_Lesson_Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _49_Lesson_Task1
+{
+    class Program
+    {
+        static void PrintArray(int[] arr, int i = 0)
+        {
+            
+            if (i < arr.Length)
+            {
+                Console.WriteLine(arr[i]);
+                PrintArray(arr, i + 1);
+            }
+               
+
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray= { 1, 2, 3, 5, 10, 100, 200};
+           
+            PrintArray(myArray);
+
+        }
+    }
+}
