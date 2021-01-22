9c4e2c5dd1429b01ee90711a503a2e04d6d1889f
Andrei Tsimakhovich <taa421992@gmail.com>
2021-01-22 11:46:15 +0300

    Add 50_Lesson. Rename solution from Training to C_sharp_Lessons.

---
+++ 50_Lesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _50_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
