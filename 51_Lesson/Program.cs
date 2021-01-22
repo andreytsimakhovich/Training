b646c571f859e1abbe055f7f80444901bbdf3b56
Andrei Tsimakhovich <taa421992@gmail.com>
2021-01-22 15:50:37 +0300

    Add 51_Lesson.

---
+++ 51_Lesson/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+namespace _51_Lesson
+{
+    // "Ядерный Ганди" и арифметическое переполнение
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte aggression = 1;
+
+            byte democracyModifier = 2;
+
+
+            try
+            {
+                aggression = checked((byte)(aggression - democracyModifier)); //сужающее преобразование данных
+
+                Console.WriteLine(aggression);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Ошибка: арифметическое переполнение!");
+            }
+
+           
+        }
+    }
+}
---
+++ 51_Lesson2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _51_Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.MaxValue;
+
+            Console.WriteLine("Введите значение");
+            int c = int.Parse(Console.ReadLine()); 
+
+            a = checked (a + c); //переполнение через верхнюю границу
+            Console.WriteLine(a);
+
+            int b = int.MinValue;
+            b = b - 1; //переполнение через нижнюю границу
+            Console.WriteLine(b);
+        }
+    }
+}
