895910db003255255c6ef48314039aab27c24364
Andrei Tsimakhovich <taa421992@gmail.com>
2021-01-26 09:50:59 +0300

    Add 53_Lesson.

---
+++ 53_Lesson/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _53_Lesson
+{
+    // Ключевое слово var
+    // неявно типизированные локальные переменные
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = 5;
+            Console.WriteLine(t.GetType());
+        }
+    }
+}
