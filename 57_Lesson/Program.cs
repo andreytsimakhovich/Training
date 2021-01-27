4f48420aa24e295b9316788e5e88ace39ecfebe4
Andrei Tsimakhovich <taa421992@gmail.com>
2021-01-27 15:43:17 +0300

    Add 57_Lesson.

---
+++ 57_Lesson/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using System.Reflection;
+
+namespace _57_Lesson
+{
+    // модификаторы доступа public и private, для членов класса
+    class Point 
+    {
+        int z = 5;
+
+        public int x = 1;
+
+        private int y = 44;
+
+        private void PrintX()
+        {
+            Console.WriteLine($"X: {x}");
+        }
+
+        public void PrintY()
+        {
+            Console.WriteLine($"Y: {y}");
+        }
+
+        public void PrintPoint()
+        {
+            PrintY();
+            PrintX();
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var typeInfo = typeof(Point).GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+            foreach (var item in typeInfo)
+            {
+                Console.WriteLine($"{item.Name}\t IsPrivate: {item.IsPrivate}\t IsPublic: {item.IsPublic}");
+            }
+
+        }
+    }
+}
