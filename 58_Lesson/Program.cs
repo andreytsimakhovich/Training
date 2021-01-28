760f772d4b376f126a1e13bda72448f5c327491d
Andrei Tsimakhovich <taa421992@gmail.com>
2021-01-28 11:35:45 +0300

    Add 59_Lesson.

---
+++ 58_Lesson/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _58_Lesson
+{
+    // Инкапсуляция
+    class Program
+    {
+
+        class Gun
+        {
+            private bool isLoaded;
+            private void Reload()
+            {
+                Console.WriteLine("Заряжаю...");
+
+                isLoaded = true;
+
+                Console.WriteLine("Заряжено!");
+            }
+
+            public void Shoot()
+            {
+                if (!isLoaded)
+                {
+                    Console.WriteLine("Орудие не заряжено!");
+                    Reload();
+                }
+
+                Console.WriteLine("Ба-бах!\n");
+                isLoaded = false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Gun gun = new Gun();
+
+            // gun.isLoaded = true;
+            gun.Shoot();
+        }
+    }
+}
---
+++ 59_Lesson/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _59_Lesson
+{
+    class Program
+    {
+        // Конструктор класса
+        // Конструктор по умолчанию
+        class Gun
+        {
+
+            public Gun(bool isLoaded)
+            {
+                _isLoaded = isLoaded;
+            }
+
+            private bool _isLoaded;
+            private void Reload()
+            {
+                Console.WriteLine("Заряжаю...");
+
+                _isLoaded = true;
+
+                Console.WriteLine("Заряжено!");
+            }
+
+            public void Shoot()
+            {
+                if (!_isLoaded)
+                {
+                    Console.WriteLine("Орудие не заряжено!");
+                    Reload();
+                }
+
+                Console.WriteLine("Ба-бах!\n");
+                _isLoaded = false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Gun gun = new Gun(isLoaded:true);
+            gun.Shoot();
+        }
+    }
+}
